dbc65840176867ece84025b6b66a305c51930b48
LuisAW <luis.wsky@gmail.com>
2022-03-15 21:05:17 -0300

    Initial commit

---
+++ calculos.py
@@ -0,0 +1,8 @@
+# Cálculos
+nome = str(input('Olá, como se chama?'))
+print('Legal, {}, Vamos para os cálculos?'.format(nome))
+n1 = int(input('Digite um número:'))
+print('O dobro desse número é {}:'.format(n1*2))
+print('O triplo desse número é {}:'.format(n1*3))
+print('A raiz quadrada desse número é {}:'.format(n1**(1/2)))
+print('Agora chega, foi bom enquanto durou!')
